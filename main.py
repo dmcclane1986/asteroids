4a9776fac51fc82b63ca27e00ddd0cd6d3d810ab
dmcclane1986 <dmcclane1986@gmail.com>
2025-03-12 14:14:42 -0500

    Added player triangle and rotating

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     game_on = True
+    fps_clock = pygame.time.Clock()
+    dt = 0
     while game_on:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = fps_clock.tick(60)/1000
 
 
 
