fa3931d0eb40960640dae239a91d7b59712ee3bd
dmcclane1986 <dmcclane1986@gmail.com>
2025-03-12 15:11:27 -0500

    added asteriods and hitting them will close game

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 
 def main():
@@ -13,10 +16,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
 
@@ -26,6 +33,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteriod in asteroids:
+            if asteriod.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
